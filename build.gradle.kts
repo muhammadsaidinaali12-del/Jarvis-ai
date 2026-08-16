7b63c9460892cae80b608b807228d6f3f593e9f6
muhammadsaidinaali12-del <muhammadsaidinaali12@gmail.com>
2026-08-17 04:53:11 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
-android {
-    namespace = "com.example"
-    ...
-}
-
-dependencies {
-    ...
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.roborazzi) apply false
+    alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.google.services) apply false
 }
